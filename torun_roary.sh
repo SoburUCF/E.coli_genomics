81767ac6e3490ede4012c32b1a53ee859e85f0a4
Sobur Ali <66832241+SoburUCF@users.noreply.github.com>
2024-01-02 13:48:57 -0500

    Add files via upload

---
+++ torun_roary.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#SBATCH -N 1
+#SBATCH -n 18
+#SBATCH -t 6-00
+#SBATCH --mem=10000
+#SBATCH -o roary.%N.%j.out
+#SBATCH -e roary.%N.%j.err
+
+#Run roary for pangenome analysis
+roary -p $SLURM_NTASKS -o {1} -i 90 -e --mafft --dont_delete_files -v *.gff
+
+#Using SNP-sites to extract only polymorphic sites from a Roary output file
+snp-sites -o SNPs.fasta -c core_gene_alignment.aln
+
+#Run IQ tree to create phylogeny
+#SNP/Ascertainment bias correction with IQ-TREE and automatic model selection (this method is almost as accurate as RAxML as less computationally taxing) - bootstrapping can be added as well
+iqtree -s SNPs.fasta -m MFP+ASC -nt AUTO
